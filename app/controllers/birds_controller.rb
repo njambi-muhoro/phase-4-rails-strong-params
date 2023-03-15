12963ed4dd456b644b01a6cef75cf123394e18ee
+ <+>
2023-03-16 00:33:08 +0300

    Assignment Done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,4 +22,19 @@ class BirdsController < ApplicationController
     end
   end
 
+    # POST /birds
+  def create
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
+  # other controller actions here
+
+  private
+  # all methods below here are private
+
+  def bird_params
+    params.permit(:name, :species)
+  end
+
 end
